74e9fac52a7d3b70fd1845fd48d9b395618085b0
PTrillat <63075218+PTrillat@users.noreply.github.com>
2024-01-01 22:16:52 +0100

    Add print to layer in main.cpp

--- main.cpp
+++ main.cpp
@@ -3,35 +3,56 @@
 #include <vector>
 #include <fstream>
 
+float huberLoss(float x) {
+    return std::sqrt(x * x + 1.0f) - 1.0f;
+}
+
+float hberLossDerivative(float x) {
+    return x / std::sqrt(x * x + 1.0f);
+}
+
 template<int l, int m, int n> // dimensions
 struct Layer {
     // Eigen::Matrix<float, l, m> X, dX;
     Eigen::Matrix<float, m, n> A, dA;
     Eigen::Matrix<float, 1, n> B, dB;
     Eigen::Matrix<float, l, n> Y, dY;
-    // activation function + derivative
+    // real numeric activation function and derivative
+    float (*f)(float);
+    float (*df)(float);
 
-    void initialise() {
+    void print(std::ostream& os)
+    {
+        os << "A :\n" << A << std::endl;
+        os << "dA :\n" << dA << std::endl;
+        os << "B :\n" << B << std::endl;
+        os << "dB :\n" << dB << std::endl;
+        os << "Y :\n" << Y << std::endl;
+        os << "dY :\n" << dY << std::endl;
+    }
+
+    void initialise(float (*f)(float), float (*df)(float)) {
         A = Eigen::Matrix<float, m, n>::Random();
         dA.setZero();
         B = Eigen::Matrix<float, 1, n>::Random();
         dB.setZero();
-        Y = Eigen::Matrix<float, l, n>::Random();
+        Y.setZero();
         dY.setZero();
+        this->f = f;
+        this->df = df;
     }
 
     void propagation(const Eigen::Matrix<float, l, m> &X) {
         Y = (X * A).rowwise() + B; // for now this is a Z matrix
-        // dY = Y.array().df()
-        // Y = Y.array().exp()
+        dY = Y.array().exp(); // (&f);
+        Y = Y.array().exp(); // (&df); // now activation is applied
     }
 
     void retropagation(const Eigen::Matrix<float, l, m> &X, Eigen::Matrix<float, l, m> &dX) {
-        // Supposes that dY has been updated correctly, and propagates this accordingly
+        // Supposes that dY has been updated correctly, and propagates this assumption
         dB = dY.colwise().sum();
         dA = X.transpose() * dY;
-        // dX (from the previous layer) contrains f'(Z) and d(Cost)/dZ = d(cost)/dY * dY/dZ
-        // dX *= dY * A.transpose();
+        // dX *= dY * A.transpose(); // not to forget 
     }
 };
 
@@ -46,27 +67,12 @@ int main()
     std::cout << "dX : " << dX << std::endl;
 
     Layer<l, m, n> layer;
-    layer.initialise();
-    std::cout << "A : " << layer.A << std::endl;
-    std::cout << "dA : " << layer.dA << std::endl;
-    std::cout << "B : " << layer.B << std::endl;
-    std::cout << "dB : " << layer.dB << std::endl;
-    std::cout << "Y : " << layer.Y << std::endl;
-    std::cout << "dY : " << layer.dY << std::endl;
+    layer.initialise(huberLoss, hberLossDerivative);
+    layer.print(std::cout);
 
     layer.propagation(X);
-    std::cout << "A : " << layer.A << std::endl;
-    std::cout << "dA : " << layer.dA << std::endl;
-    std::cout << "B : " << layer.B << std::endl;
-    std::cout << "dB : " << layer.dB << std::endl;
-    std::cout << "Y : " << layer.Y << std::endl;
-    std::cout << "dY : " << layer.dY << std::endl;
+    layer.print(std::cout);
 
     layer.retropagation(X, dX);
-    std::cout << "A : " << layer.A << std::endl;
-    std::cout << "dA : " << layer.dA << std::endl;
-    std::cout << "B : " << layer.B << std::endl;
-    std::cout << "dB : " << layer.dB << std::endl;
-    std::cout << "Y : " << layer.Y << std::endl;
-    std::cout << "dY : " << layer.dY << std::endl;
+    layer.print(std::cout);
 }
