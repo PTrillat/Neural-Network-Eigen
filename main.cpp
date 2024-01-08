79a895628c1a6b2619c4b7f50f23cbc9cf310f17
PTrillat <63075218+PTrillat@users.noreply.github.com>
2024-01-08 17:42:52 +0100

    Add second layer in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,78 +1,152 @@
-#include <iostream>
 #include <Eigen/Dense>
-#include <vector>
 #include <fstream>
+#include <iostream>
+#include <vector>
 
-float huberLoss(float x) {
-    return std::sqrt(x * x + 1.0f) - 1.0f;
+template <int m, int n> // dimensions
+void pretty_print(std::ostream &os, const std::string name,
+                  const Eigen::Matrix<float, m, n> &M,
+                  const Eigen::Matrix<float, m, n> &dM) {
+  os << name << std::endl;
+  for (int i = 0; i < m; i++) {
+    os << '\t' << M(i, 0);
+    for (int j = 1; j < n; j++)
+      os << ", " << M(i, j);
+    os << '\t' << dM(i, 0);
+    for (int j = 1; j < n; j++)
+      os << ", " << dM(i, j);
+    os << std::endl;
+  }
 }
 
-float hberLossDerivative(float x) {
-    return x / std::sqrt(x * x + 1.0f);
+/* sigmoid activation bloc */
+template <int l, int m, int n>
+void sigmoid(Eigen::Matrix<float, l, n> &Z, Eigen::Matrix<float, l, n> &dZ) {
+  Eigen::Array<float, l, n> sigma = 1.f / (1.f + Eigen::exp(-Z.array()));
+  Z = sigma.matrix();
+  dZ = (sigma * (1.f - sigma)).matrix();
 }
 
-template<int l, int m, int n> // dimensions
+/* relu activation bloc */
+template <int l, int m, int n>
+void rectified(Eigen::Matrix<float, l, n> &Z, Eigen::Matrix<float, l, n> &dZ) {
+  const float alpha = .01f; // how leaky it is
+  Eigen::Array<float, l, n> sigma =
+      .5f * ((1.f - alpha) * Eigen::sign(Z.array()) +
+             (1.f + alpha)); // leaky positive
+  dZ = sigma.matrix();
+  Z.array() = sigma * dZ.array();
+}
+
+/* Squared L2 norm loss */
+template <int l, int n>
+float squared(const Eigen::Matrix<float, l, n> &Y,
+              const Eigen::Matrix<float, l, n> &Yref,
+              Eigen::Matrix<float, l, n> &dY,
+              bool silent) {
+    const Eigen::Matrix<float, l, n> diff = Y - Yref;
+    dY.array() *= diff.array();
+    if (silent)
+        return 0.f;
+    return .5f * diff.squaredNorm();
+
+}
+
+/* Huber Loss bloc */
+float huberLoss(float x) { return std::sqrt(x * x + 1.0f) - 1.0f; }
+
+float huberLossDerivative(float x) { return x / std::sqrt(x * x + 1.0f); }
+
+
+template <int l, int m, int n> // dimensions
 struct Layer {
-    // Eigen::Matrix<float, l, m> X, dX;
-    Eigen::Matrix<float, m, n> A, dA;
-    Eigen::Matrix<float, 1, n> B, dB;
-    Eigen::Matrix<float, l, n> Y, dY;
-    // real numeric activation function and derivative
-    float (*f)(float);
-    float (*df)(float);
+  // Eigen::Matrix<float, l, m> X, dX;
+  Eigen::Matrix<float, m, n> A, dA;
+  Eigen::Matrix<float, 1, n> B, dB;
+  Eigen::Matrix<float, l, n> Y, dY;
+  // compute activation function and derivative
+  // should modify the inputs arrays inplace
+  void (*activation)(Eigen::Matrix<float, l, n> &Z,
+                     Eigen::Matrix<float, l, n> &dZ);
 
-    void print(std::ostream& os)
-    {
-        os << "A :\n" << A << std::endl;
-        os << "dA :\n" << dA << std::endl;
-        os << "B :\n" << B << std::endl;
-        os << "dB :\n" << dB << std::endl;
-        os << "Y :\n" << Y << std::endl;
-        os << "dY :\n" << dY << std::endl;
-    }
+  void print(std::ostream &os) const {
+    os << std::scientific;
+    pretty_print(os, "A", A, dA);
+    pretty_print(os, "B", B, dB);
+    pretty_print(os, "Y", Y, dY);
+  }
 
-    void initialise(float (*f)(float), float (*df)(float)) {
-        A = Eigen::Matrix<float, m, n>::Random();
-        dA.setZero();
-        B = Eigen::Matrix<float, 1, n>::Random();
-        dB.setZero();
-        Y.setZero();
-        dY.setZero();
-        this->f = f;
-        this->df = df;
-    }
+  void initialise(const char *act) {
+    A = Eigen::Matrix<float, m, n>::Random();
+    B = Eigen::Matrix<float, 1, n>::Random();
+    Y.setZero();
+    dA.setZero();
+    dB.setZero();
+    dY.setZero();
+    if (act == "sigmoid")
+      this->activation = sigmoid<l, m, n>;
+    else
+      this->activation = rectified<l, m, n>;
+  }
 
-    void propagation(const Eigen::Matrix<float, l, m> &X) {
-        Y = (X * A).rowwise() + B; // for now this is a Z matrix
-        dY = Y.array().exp(); // (&f);
-        Y = Y.array().exp(); // (&df); // now activation is applied
-    }
+  void propagation(const Eigen::Matrix<float, l, m> &X) {
+    Y = (X * A).rowwise() + B; // for now this is a Z matrix
+    activation(Y, dY);         // element wise operations
+  }
 
-    void retropagation(const Eigen::Matrix<float, l, m> &X, Eigen::Matrix<float, l, m> &dX) {
-        // Supposes that dY has been updated correctly, and propagates this assumption
-        dB = dY.colwise().sum();
-        dA = X.transpose() * dY;
-        // dX *= dY * A.transpose(); // not to forget 
-    }
+  void retropagation(const Eigen::Matrix<float, l, m> &X,
+                     Eigen::Matrix<float, l, m> &dX) {
+    // Supposes that dY constains activation derivative
+    // and computes full derivative based on this assumption
+    dB = dY.colwise().sum();
+    dA = X.transpose() * dY;
+    dX.array() *= (dY * A.transpose()).array(); // element wise multiplication
+  }
+
+  void update(const float step) {
+    A -= step * dA;
+    B -= step * dB;
+  }
 };
 
-const int l = 5, m = 3, n = 1;
- 
-int main()
-{
-    Eigen::Matrix<float, l, m> X, dX;
-    X.setZero();
-    dX.setZero();
-    std::cout << "X : " << X << std::endl;
-    std::cout << "dX : " << dX << std::endl;
+const int l = 4, m = 2, n = 1;
 
-    Layer<l, m, n> layer;
-    layer.initialise(huberLoss, hberLossDerivative);
-    layer.print(std::cout);
+int main() {
+  Eigen::Matrix<float, l, m> X, dX;
+  Eigen::Matrix<float, l, n> Y, dY;
+  X(0, 0) = 0.f; X(0, 1) = 0.f; Y(0) = 1.f;
+  X(1, 0) = 0.f; X(1, 1) = 1.f; Y(1) = 1.f;
+  X(2, 0) = 1.f; X(2, 1) = 0.f; Y(2) = 1.f;
+  X(3, 0) = 1.f; X(3, 1) = 1.f; Y(3) = 0.f;
 
-    layer.propagation(X);
-    layer.print(std::cout);
+  pretty_print(std::cout, "X", X, dX);
+  pretty_print(std::cout, "Y", Y, dY);
 
-    layer.retropagation(X, dX);
-    layer.print(std::cout);
+
+  Layer<l, m, 2> layer1;
+  Layer<l, 2, n> layer2;
+  
+  layer1.initialise("sigmoid");
+  layer2.initialise("sigmoid");
+  
+  layer1.print(std::cout);
+  layer2.print(std::cout);
+
+  for (int epoch = 0; epoch < 10; epoch++) {
+    for (int batch = 0; batch < 1000; batch++) {
+        layer1.propagation(X);
+        layer2.propagation(layer1.Y);
+        
+        squared(layer2.Y, Y, layer2.dY, true);
+        
+        layer2.retropagation(layer1.Y, layer1.dY);
+        layer1.retropagation(X, dX);
+        
+        layer1.update(.01f);
+        layer2.update(.01f);
+      }
+    std::cout << "error : " << squared(layer2.Y, Y, layer2.dY, false) << std::endl;
+  }
+  layer1.print(std::cout);
+  layer2.print(std::cout);
 }
